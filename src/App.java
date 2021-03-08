028527291be1c0ac4ce2787b0e89de40d65ceda9
rayalva407 <rayalva407@gmail.com>
2021-03-07 23:40:02 -0700

    creates simple cli

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner (System.in);
+        double fnum, snum, answer;
+        System.out.println("Enter first number: ");
+        fnum = in.nextDouble();
+        System.out.println("Enter second number: ");
+        snum = in.nextDouble();
     }
 }
